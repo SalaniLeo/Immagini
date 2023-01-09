e8144d3e89d4ce9891bd549416ce2db7806f6aac
SalaniLeo <leonardo07.salani@gmail.com>
2023-01-09 23:23:59 +0100

    modified Gtkfilechooser

--- main.py
+++ main.py
@@ -12,37 +12,12 @@ class Handler:
         
 # opens popup window for exe selection
     def chooseExe(self, button):
-        dialog = Gtk.FileChooserDialog(title="choose the executable",parent=None)
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(500, 500)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            imageCreator.exe = dialog.get_filename()
-            exeEntry.set_text(dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            None
-
-        dialog.destroy()
+        exeEntry.set_text(fileChooser("Choose executable file", False))
 
 # opens popup window for icon selection
     def chooseIcon(self, button):
-        dialog = Gtk.FileChooserDialog(title="choose the icon",parent=None)
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(500, 500)
+        iconEntry.set_text(fileChooser("Choose executable file", False, True))
 
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            imageCreator.icon = dialog.get_filename()
-            iconEntry.set_text(dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            None
-
-        dialog.destroy()
 
 
 # saves and switches page for the main window
@@ -57,13 +32,13 @@ class Handler:
             secondPage.set_title("AppImageCreator")
         
             
-            if(builder.get_object("folderMSwitch").get_active()):
+            if(folderMSwitch.get_active()):
                 
                 spHandler.AdvOption("folderMLabel",True)
                 spHandler.AdvOption("folderMEntry",True)
                 spHandler.AdvOption("folderMButton",True)
                 
-            if(builder.get_object("CustomARSwitch").get_active()):
+            if(CustomARSwitch.get_active()):
                 
                 spHandler.AdvOption("AppRLabel",True)
                 spHandler.AdvOption("AppREntry",True)
@@ -89,56 +64,80 @@ class Handler:
 
 # hanlder of the other window
 class spHandler():
+    
 
-    # opens folder chooser dialog
-    def selectOutput(self, button):
-        dialog = Gtk.FileChooserDialog(title="choose the icon",parent=None, action=Gtk.FileChooserAction.SELECT_FOLDER)
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(500, 500)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            Handler.updateLoc(dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            None
-
-        dialog.destroy()
-
-    # starts creating the app
+        # starts creating the app
     def startCreating(self, button):
-
+        
         expander = sp.get_object("expander")
         expander.set_expanded(True)
         outputConsole = sp.get_object("outputConsole")
         tb = outputConsole.get_buffer()
         end_iter = tb.get_end_iter()
-        tb.insert(end_iter, imageCreator.start(nameEntry.get_text(),exeEntry.get_text(),iconEntry.get_text(),typeEntry.get_text(),categoriesEntry.get_text(),sp.get_object("outputEntry").get_text())) 
+        tb.insert(end_iter, imageCreator.start(
+            
+            nameEntry.get_text(),
+            exeEntry.get_text(),
+            iconEntry.get_text(),
+            typeEntry.get_text(),
+            categoriesEntry.get_text(),
+            sp.get_object("outputEntry").get_text(),
+            CustomARSwitch.get_active(),
+            sp.get_object("AppREntry").get_text()
+            
+            )) 
         # outputConsole.set_hexpand(True)
         remAppDir = sp.get_object("remAppDir")
-
         if(remAppDir.get_active()):
             shutil.rmtree(sp.get_object("outputEntry").get_text() + "/" + nameEntry.get_text() + ".AppDir/")
 
+
+    # opens folder chooser dialog
+    def selectOutput(self, button):
+        outputEntry = sp.get_object("outputEntry")
+        outputEntry.set_text(fileChooser("choose output location", True))
+        
+    def chooseParentFolder(self, button):
+        folderMEntry = sp.get_object("folderMEntry")
+        folderMEntry.set_text(fileChooser("select app parent folder", True))
+        
+    def chooseAppRun(self, button):
+        AppREntry = sp.get_object("AppREntry")
+        AppREntry.set_text(fileChooser("choose AppRun file", False))
+        
     def AdvOption(element, show):
         final = sp.get_object(element)
         final.set_visible(show)
+        
 
-def fileChooser():
+def fileChooser(title, folderOnly=Gtk.FileChooserAction.OPEN,iconFilter=False):
     
-        dialog = Gtk.FileChooserDialog(title="choose the icon",parent=None)
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
+        if(folderOnly):
+            folderOnly = Gtk.FileChooserAction.SELECT_FOLDER
+    
+        dialog = Gtk.FileChooserDialog(title=title,action=folderOnly,parent=None)
+        dialog.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK)
         dialog.set_default_size(500, 500)
+        dialog.set_current_folder("/home/leo/Desktop")
+
+        if(iconFilter):
+            filter_text = Gtk.FileFilter()
+            filter_text.set_name("icon")
+            filter_text.add_pattern("*.svg")
+            filter_text.add_pattern("*.png")
+            filter_text.add_pattern("*.jpg")
+            dialog.add_filter(filter_text)
 
         response = dialog.run()
         if response == Gtk.ResponseType.OK:
-            return dialog.get_filename()
+            file = dialog.get_filename()
+            dialog.destroy()
+            if(file is None):
+                file = " "
+            return file
         elif response == Gtk.ResponseType.CANCEL:
-            None
-
+            dialog.destroy()
+                    
 # ui.errorWindow.error_message("could not copy the exe file")
 # ui.errorWindow.error_message("could not copy icon")
 
@@ -155,5 +154,6 @@ typeEntry = builder.get_object('typeEntry')
 categoriesEntry = builder.get_object('categoriesEntry')
 AdvancedOSwitch = builder.get_object('AdvancedOSwitch')
 AdvancedOGrid = builder.get_object('AdvancedOGrid')
-
+folderMSwitch = builder.get_object("folderMSwitch")
+CustomARSwitch = builder.get_object("CustomARSwitch")
 Gtk.main()

bd9ea5231f278e140a6fefa115dc11a459b22102
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2023-01-29 21:13:07 +0100

    update

--- flake/creator/error.py
+++ flake/creator/error.py
@@ -1,14 +1,10 @@
 
 import gi
-import shutil
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
 
 def throwError(self, error, title):
-        
-        # parent = Gtk.ApplicatioWindow()
-        
-        
+                 
         dialog = Gtk.MessageDialog(
                 parent         = self,
                 message_type   = Gtk.MessageType.ERROR,
@@ -16,7 +12,5 @@ def throwError(self, error, title):
                 text           = title,
                 buttons        = Gtk.ButtonsType.CLOSE,
         )
-        
+                
         dialog.show()
-        # outputConsole.set_hexpand(True)
-        return None

de1185c62993576aa2bdfef318d0393446f2493e
SalaniLeo <97695689+SalaniLeo@users.noreply.github.com>
2023-02-03 23:35:38 +0100

    removed duplicated and useless import

--- start.py
+++ start.py
@@ -1,11 +1,8 @@
 import sys
-import importlib.util
-import sys
-
 
 if __name__ == '__main__':
 
     from gi.repository import Gio
 
     from data import main
-    sys.exit(main.main("0.0.3"))
+    sys.exit(main.main("0.0.3")) 
